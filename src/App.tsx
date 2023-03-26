7a6ed1c0bb3c941746039d3f1c0f578aeb4b50ec
Daniel Chattan <dnchattan@gmail.com>
2023-03-25 23:15:57 -0400

    update site info

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Layout, theme } from 'antd';
+import { Button, Layout, Space, theme } from 'antd';
 import { HighlightOutlined } from '@ant-design/icons';
 import { useAppModel } from './Model';
 import { TeamView, TurnSimulatorView } from './Views';
@@ -20,10 +20,30 @@ export const App: React.FC = () => {
     >
       <Layout.Content className="full-height">
         <Layout className="full-height">
-          <Layout.Header style={{ background: 'transparent' }}>
-            <Button icon={<HighlightOutlined />} onClick={dispatch.changeTheme}>
-              Change theme
-            </Button>
+          <Layout.Header
+            style={{ background: 'transparent', display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+          >
+            <div style={{ flex: 1 }} />
+            <Space.Compact>
+              <Button
+                type="dashed"
+                icon={
+                  <img
+                    src="/logo.png"
+                    alt="Raid Toolkit Logo"
+                    style={{ objectFit: 'scale-down', height: '1rem', paddingRight: 4 }}
+                  />
+                }
+                target="_blank"
+                style={{ fontSize: 12, fontVariant: 'all-small-caps' }}
+                href="https://raidtoolkit.com"
+              >
+                Powered by: Raid Toolkit
+              </Button>
+              <Button icon={<HighlightOutlined />} onClick={dispatch.changeTheme}>
+                Change theme
+              </Button>
+            </Space.Compact>
           </Layout.Header>
           <Layout.Content className="full-height">
             <div style={{ height: '100%', overflowY: 'auto', position: 'relative', zIndex: 1 }}>
