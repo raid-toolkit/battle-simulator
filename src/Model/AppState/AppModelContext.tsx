46fae4233aaa302ab8aea4c49fb444278c9de389
Daniel Chattan <dnchattan@gmail.com>
2023-07-29 02:11:28 -0400

    reset setting values

--- src/Model/AppState/AppModelContext.tsx
+++ src/Model/AppState/AppModelContext.tsx
@@ -21,14 +21,14 @@ const defaultTune: TuneState = {
   championList: [],
 };
 
-const initialTurnLimit = safeLocalStorage.getItem('v1_turn_limit');
-const initialBossTurnLimit = safeLocalStorage.getItem('v1_boss_turn_limit');
+const initialTurnLimit = safeLocalStorage.getItem('v2_turn_limit');
+const initialBossTurnLimit = safeLocalStorage.getItem('v2_boss_turn_limit');
 
 function useAppModelInternal(): AppModel {
   const [state, setState] = useImmer<AppState>({
     theme: 'dark',
     turnLimit: initialTurnLimit ? parseInt(initialTurnLimit, 10) : 6,
-    bossTurnLimit: initialBossTurnLimit ? parseInt(initialBossTurnLimit, 10) : 75,
+    bossTurnLimit: initialBossTurnLimit ? parseInt(initialBossTurnLimit, 10) : 40,
     visiblePanel: 'team',
     infoDialogTab: undefined,
     settingsVisible: false,
@@ -130,14 +130,14 @@ function useAppModelInternal(): AppModel {
         }
 
         setTurnLimit(turnLimit: number) {
-          safeLocalStorage.setItem('v1_turn_limit', turnLimit.toString());
+          safeLocalStorage.setItem('v2_turn_limit', turnLimit.toString());
           setState((state) => {
             state.turnLimit = turnLimit;
           });
         }
 
         setBossTurnLimit(turnLimit: number) {
-          safeLocalStorage.setItem('v1_boss_turn_limit', turnLimit.toString());
+          safeLocalStorage.setItem('v2_boss_turn_limit', turnLimit.toString());
           setState((state) => {
             state.bossTurnLimit = turnLimit;
           });
